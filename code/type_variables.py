5b8339c80f6c60cbf96530c99a484ba501b4778d
IdonthaveanUsernam <alexandrehallynck@gmail.com>
2025-04-21 18:51:58 +0200

    Create type_variables.py

---
+++ code/type_variables.py
@@ -0,0 +1,14 @@
+whole_number = 89 #int
+float_number = 89.464546786 #float
+random_text = "jguihjiosrpoiujgo" #string
+boolean_value = True #boolean
+
+type1 = type(whole_number)
+type2 = type(float_number)
+type3 = type(random_text)
+type4 = type(boolean_value)
+
+print(type1)
+print(type2)
+print(type3)
+print(type4)
