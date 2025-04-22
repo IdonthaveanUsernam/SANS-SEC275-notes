e278decf28df2c4ba06fadbdce921f6a6e725c25
IdonthaveanUsernam <alexandrehallynck@gmail.com>
2025-04-22 15:55:55 +0200

    Create data.py

---
+++ code/data.py
@@ -0,0 +1,21 @@
+print(ord('A'))
+print(ord('b'))
+print(ord('Z'))
+#ord give you the decimal representation
+
+print(chr(65))
+print(chr(69))
+print(chr(72))
+#chr give you the character representation
+
+print(hex(ord('A')))
+print(hex(ord('b')))
+print(hex(ord('Z')))
+#This give you the hexadecimal representation
+
+print(bin(ord('A')))
+print(bin(ord('b')))
+print(bin(ord('Z')))
+#give you the binary representation
+
+#Note that 0b before a number mean binary and 0x before a number mean hexadecimal
