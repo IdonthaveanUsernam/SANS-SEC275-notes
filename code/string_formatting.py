9092c52bfeaf37e995776143970338333c16f1d0
IdonthaveanUsernam <alexandrehallynck@gmail.com>
2025-04-22 15:47:51 +0200

    Create string_formatting.py

---
+++ code/string_formatting.py
@@ -0,0 +1,14 @@
+first_name = "Billy"
+last_name = "Joe"
+age = 65
+
+print('My name is %s, %s and i\'m %d years old' %(first_name,last_name,age))#%s for placing string
+#%d for placing digit values
+
+age = 65.69
+print('Actually my age is %.2f' %(age))
+#%.2f mean two digit after the dot
+age = 65
+print('My age in hexadecimal is: %x' %(age))
+print('My age in octal is: %o' %(age))
+                                           
