695a56ae1efeaa31c418bc654904a8389cbe3bf6
IdonthaveanUsernam <alexandrehallynck@gmail.com>
2025-04-22 09:14:03 +0200

    Create whitespace.py

---
+++ code/whitespace.py
@@ -0,0 +1,6 @@
+list_of_friends = 'George\nJaxon\nLila\nMilo'
+print(list_of_friends)
+list_of_friends = 'George\tJaxon\tLila\tMilo'
+print(list_of_friends)
+list_of_friends = '\n\tGeorge\n\tJaxon\n\tLila\n\tMilo'
+print(list_of_friends)
