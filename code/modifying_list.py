2806ebdcb1fedce1623ea529f59f868ec84d548f
IdonthaveanUsernam <alexandrehallynck@gmail.com>
2025-04-22 17:17:56 +0200

    Create modifying_list.py

---
+++ code/modifying_list.py
@@ -0,0 +1,21 @@
+my_fruits = ['banana', 'mango', 'apple']
+print(my_fruits)
+
+my_fruits[2] = 'Pineapple' #change the element at indexe 2
+print(my_fruits)
+
+
+my_fruits.append('apple') #add an element at the end of the list
+print(my_fruits)
+
+my_fruits.sort() #sort the list by alphabetical order
+print(my_fruits)
+
+my_fruits.insert(1,'blueberry') #insert an element at the index 1
+print(my_fruits)
+
+my_fruits.remove('banana')  #remove the element banana from the list
+print(my_fruits) 
+
+my_fruits.pop() #remove the last element of the list
+print(my_fruits)
