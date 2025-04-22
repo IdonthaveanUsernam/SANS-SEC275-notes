f9672169f634c94094a149cfbfab48ed8b0c824b
IdonthaveanUsernam <alexandrehallynck@gmail.com>
2025-04-22 15:21:09 +0200

    Create integers.py

---
+++ code/integers.py
@@ -0,0 +1,6 @@
+print(2+3) #add
+print(4-3) #substract
+print(4/2) #divide
+print(4*2) #multiply
+print(4**2) #** is exponential operator
+print(13%2) # % operator give the remainder left after a divison
