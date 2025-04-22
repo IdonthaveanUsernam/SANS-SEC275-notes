0e01eb9ee58e6370a49f16c6d1cb503bb2103a7d
IdonthaveanUsernam <alexandrehallynck@gmail.com>
2025-04-22 16:13:29 +0200

    Create list.py

---
+++ code/list.py
@@ -0,0 +1,11 @@
+my_fruits = ['banana', 'apple', 'melon', 'mango', 'blueberry']
+print(my_fruits[0]) #indexes for a list start at 0
+print(my_fruits[1])
+#print(my_fruit[5] this is comment ou because 5 take the 6th element of the list but it does not exist and you will get an error
+print(my_fruits[-1]) #you can start from the left side at index -1 
+print(my_fruits[-2]) 
+
+
+random = ['Mango', 89.89, ['apple', 'banana'], 'chicken'] #you can have anything in you're list
+print(random[2][1]) #you want first to go into the first list and then the second list
+
