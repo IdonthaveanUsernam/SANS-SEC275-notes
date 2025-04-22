7f341b99b779467e9035cd78bce766b2f4556259
IdonthaveanUsernam <alexandrehallynck@gmail.com>
2025-04-22 09:06:16 +0200

    Create concatenating_string.py

---
+++ code/concatenating_string.py
@@ -0,0 +1,7 @@
+first_name = 'Captain'
+last_name = 'America'
+full_name = first_name + ' ' + last_name
+print(full_name)
+
+print('The best number is:', 13) # when you concatenating you'll need to
+#make sure all values are of the data type 'string', this is why I separate#them with a comma rather than concatenate
