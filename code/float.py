e982ccbbbaa5baf71195278cb04ce4c1a37ce003
IdonthaveanUsernam <alexandrehallynck@gmail.com>
2025-04-22 15:28:34 +0200

    Create float.py

---
+++ code/float.py
@@ -0,0 +1,4 @@
+print(5.6 + 4.4)
+print(0.2 +0.1) #this beahvior is due to the fact that computer can store only decimal value you can watch online for a detailed explanation
+print(round(49.4654748, 2)) #round with 2 decimals
+print(round(12.7864584, 3)) #round with 3 decimals
