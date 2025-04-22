47d119110fd22e0569ec18514edac795ee20da80
IdonthaveanUsernam <alexandrehallynck@gmail.com>
2025-04-22 15:35:42 +0200

    Create bool.py

---
+++ code/bool.py
@@ -0,0 +1,9 @@
+print(bool('Burger')) #string with a value
+print(bool('')) #string with empty value
+print(bool(879)) #integer with a value
+print(bool(0)) #integer without a value
+print(bool(-1)) #integer with a value
+print(bool("0")) #string with a value
+print(bool(True)) #boolean value of True
+print(bool(False)) #boolean value of False
+
