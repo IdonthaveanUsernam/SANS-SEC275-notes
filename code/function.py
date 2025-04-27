c88c8b92a6aef400822c91203160e626ccf465ee
IdonthaveanUsernam <alexandrehallynck@gmail.com>
2025-04-27 11:18:29 +0200

    Create function.py

---
+++ code/function.py
@@ -0,0 +1,14 @@
+def area_rectangle(x,y): #define a function with 2 parameters x and y
+    area = x*y
+    return area
+print("The area of the rectangle is:", area_rectangle(8,9)) #call the function and pass 2 arguments
+
+first_name = "Git"
+last_name = "Hub"
+
+
+def full_name(first_name, last_name): #define a function with 2 parameters
+    print("My fullname is %s, %s" %(first_name, last_name))
+
+full_name(first_name, last_name) #call the function and pass the 2 two arguments
+
