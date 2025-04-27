cd244f794220d991f8886373dcb9fb1420a941aa
IdonthaveanUsernam <alexandrehallynck@gmail.com>
2025-04-27 17:19:42 +0200

    Create conditionals.py

---
+++ code/conditionals.py
@@ -0,0 +1,22 @@
+list_of_fruits = ["apple", "banana", "mango", "strawberry", "orange", "pineapple", "blueberry", "kiwi", "watermelon", "grapes"]
+
+favorite_fruit = "watermelon"
+
+if favorite_fruit in list_of_fruits:
+    print('i love %s !' %(favorite_fruit))
+    if "apple" in list_of_fruits:
+        print('I don\'t really like apples')
+
+favorite_fruit = "banana"
+
+if favorite_fruit in list_of_fruits:
+    print('i love %s !' %(favorite_fruit))
+    if "Papaya" in list_of_fruits:
+        print('I don\'t really like Papaya')
+
+favorite_fruit = "papaya"
+
+if favorite_fruit in list_of_fruits:
+    print(' i love %s !' %(favorite_fruit))
+    if "apple" in list_of_fruits:
+        print('I don\'t really like apple')
