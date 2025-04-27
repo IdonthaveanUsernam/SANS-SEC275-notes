bd8c3d4338acb5c48db8c1c56f0151bd84707ea6
IdonthaveanUsernam <alexandrehallynck@gmail.com>
2025-04-27 16:52:02 +0200

    Create equality.py

---
+++ code/equality.py
@@ -0,0 +1,10 @@
+print(True == False)
+print(False == False)
+print(False == True)
+print(True == True)
+
+print('a' == 'a')
+print('A' == 'a')
+print('Github' == 'Github')
+print('GithuB' == 'Github')
+                            
