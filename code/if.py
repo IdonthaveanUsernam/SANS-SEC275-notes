1032ff4ca8ae14c8cb30a4d84eae0da60d83805f
IdonthaveanUsernam <alexandrehallynck@gmail.com>
2025-04-27 17:02:58 +0200

    Create if.py

---
+++ code/if.py
@@ -0,0 +1,6 @@
+computer_available_for_sale = 'yes'
+if computer_available_for_sale == 'yes':
+    print('I can buy a computer')
+computer_available_for_sale = 'no'
+if computer_available_for_sale == 'yes': #notice that the second if doesn't execute because the condition is not met
+    print('I can buy a computer')
